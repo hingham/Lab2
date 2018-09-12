93af8ddd0275fb7bdd8af7b8601e3c922dc5e0d6
PeterBucy <42074846+PeterBucy@users.noreply.github.com>
2018-09-11 23:32:29 -0700

    lab2
    Here is 5 questions about myself.

---
+++ Demo/js/app.js
@@ -0,0 +1,65 @@
+'use strict';
+
+
+var  question = prompt('do you think i am funny?');
+var response = (question );
+if(response.toUpperCase == 'Y' || 'YES') {
+       console.log(question );
+      alert('Hahahahahahahah!');
+}
+
+else if(response.toUpperCase == 'N' || 'NO') {
+      console.log(question );
+      alert('oh no!');
+}
+        
+
+var question = prompt('do you think i am funny looking?');
+var response = (question);
+if(response.toUpperCase == 'Y' || 'YES') {
+      console.log(question);
+      alert('Thanks you are the best!');
+}
+
+ else if(response.toUpperCase == 'N' || 'NO') {
+      console.log(question);
+      alert(question  + 'But I am!');
+}
+        
+var question = prompt('would you be my friend?');
+var response = (question);
+if(response.toUpperCase == 'Y' || 'YES') {
+      console.log(question);
+      alert('We will rule the world together!');
+}
+
+else if(response.toUpperCase == 'N' || 'NO') {
+      console.log(question);
+      alert('Man!');
+}
+        
+        
+var question = prompt('will you hang out with me?');
+var response = (question);
+if(response.toUpperCase == 'Y' || 'YES') {
+      console.log(question);
+      alert('lets go on an adventure!');
+}
+
+ else if(response.toUpperCase == 'N' || 'NO') {
+      console.log(question);
+      alert('OK!');
+}   
+    
+var question = prompt('tell me, do you love my site?');
+var response = (question);
+if(response.toUpperCase == 'Y' || 'YES') {
+      console.log(question);
+      alert('Good, because it is amazing!');
+}
+
+else if(response.toUpperCase == 'N' || 'NO') {
+      console.log(question);
+      alert('O!h');
+}   
+   
